17235a53105ec20eb95274014908a296582b46f2
Fernando Verly <fverly@ad.finep.gov.br>
2022-07-01 14:17:12 -0300

    Commit inicial

---
+++ src/main/java/br/com/verly/bovespafii/BovespaFiiApplication.java
@@ -0,0 +1,13 @@
+package br.com.verly.bovespafii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BovespaFiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BovespaFiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/verly/bovespafii/BovespaFiiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.verly.bovespafii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BovespaFiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
