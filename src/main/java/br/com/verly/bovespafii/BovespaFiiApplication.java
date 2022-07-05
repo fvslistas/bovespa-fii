16ad1e90ff1fdfb01cabefe9a19227690f61a42c
Fernando Verly <fverly@ad.finep.gov.br>
2022-07-05 14:17:23 -0300

    Configurando CORS

--- src/main/java/br/com/verly/bovespafii/BovespaFiiApplication.java
+++ src/main/java/br/com/verly/bovespafii/BovespaFiiApplication.java
@@ -3,15 +3,25 @@ package br.com.verly.bovespafii;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableConfigurationProperties({
-    FileStorageProperties.class
-})
+@EnableConfigurationProperties({ FileStorageProperties.class })
 public class BovespaFiiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BovespaFiiApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+			}
+		};
+	}
 }
--- src/main/java/br/com/verly/bovespafii/FiiController.java
+++ src/main/java/br/com/verly/bovespafii/FiiController.java
@@ -3,6 +3,7 @@ package br.com.verly.bovespafii;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
